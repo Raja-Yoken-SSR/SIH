8f6423c29506e0e58bda8f489012a1239ffb7410
Raja-Yoken-SSR <rajayoken072@gmail.com>
2025-08-31 14:04:10 +0530

    Update project files

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import cors from "cors";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "bustracker",
+  password: "postgres",
+  port: 5432,
+});
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 const buses = [
   { id: 'Bus 1', lat: 11.2588, lng: 75.7804 },
   { id: 'Bus 2', lat: 11.2645, lng: 75.7840 }
 ];
 
+let driverLocations = {};
 
-
+app.use(cors());
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.json());
 
 
 app.get("/",(req,res)=>{
@@ -32,10 +44,15 @@ app.post("/submit",(req,res)=>{
 })
 
 
+app.post("/update-location", (req, res) => {
+  const { driverId, lat, lng } = req.body;
+  console.log(`Driver ${driverId} Location: Latitude=${lat}, Longitude=${lng}`);
+  res.json({ status: "Location received" });
+});
 
-
-
-
+app.get("/driver-location", (req, res) => {
+  res.json(driverLocations); // send all driver locations
+});
 
 
 app.listen(port, () => {
